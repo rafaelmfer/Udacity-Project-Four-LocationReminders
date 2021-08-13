9dd7be9d25493213b9a591b0f4b6e6a307857028
rafaelmfer <rafaelmfer@hotmail.com>
2021-08-13 01:08:27 -0300

    upgrade libs and change the map style

--- app/src/androidTest/java/com/udacity/locationreminder/util/DataBindingIdlingResource.kt
+++ app/src/androidTest/java/com/udacity/locationreminder/util/DataBindingIdlingResource.kt
@@ -104,7 +104,7 @@ fun DataBindingIdlingResource.monitorActivity(activityScenario: ActivityScenario
 /**
  * Sets the fragment from a [FragmentScenario] to be used from [DataBindingIdlingResource].
  */
-fun DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<out Fragment>) {
+fun DataBindingIdlingResource.monitorFragment(fragmentScenario: FragmentScenario<Fragment>) {
     fragmentScenario.onFragment { fragment ->
         activity = fragment.requireActivity()
     }
