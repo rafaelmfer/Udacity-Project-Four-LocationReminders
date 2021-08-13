c05e05e1edc5603e8fdc7544de88ffedfba1b985
rafaelmfer <rafaelmfer@hotmail.com>
2021-08-12 23:05:29 -0300

    create RemindersDaoTest with FakeAndroidDataSource

---
+++ app/src/androidTest/java/com/udacity/locationreminder/locationreminders/data/FakeAndroidDataSource.kt
@@ -0,0 +1,47 @@
+package com.udacity.locationreminder.locationreminders.data
+
+import com.udacity.locationreminder.locationreminders.data.dto.ReminderDTO
+import com.udacity.locationreminder.locationreminders.data.dto.Result
+
+class FakeAndroidDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) : ReminderDataSource {
+
+    companion object {
+        const val REMINDER_NOT_FOUND = "Reminders could not found"
+    }
+
+    private var shouldReturnError = false
+
+    fun shouldReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
+
+    override suspend fun getReminders(): Result<List<ReminderDTO>> {
+        if (shouldReturnError) {
+            return Result.Error(REMINDER_NOT_FOUND)
+        }
+
+        reminders?.let { return Result.Success(it) }
+
+        return Result.Error(REMINDER_NOT_FOUND)
+    }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        reminders?.add(reminder)
+    }
+
+    override suspend fun getReminder(id: String): Result<ReminderDTO> {
+        val reminder = reminders?.firstOrNull { reminder ->
+            reminder.id == id
+        }
+
+        if (reminder != null) {
+            return Result.Success(reminder)
+        }
+
+        return Result.Error(REMINDER_NOT_FOUND)
+    }
+
+    override suspend fun deleteAllReminders() {
+        reminders?.clear()
+    }
+}
